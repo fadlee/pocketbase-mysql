e4e04c59f728f131c67b5a5529fccad6779551b7
fadlee <fad.lee@hotmail.com>
2026-05-10 02:45:37 +0700

    fix MySQL relation many filters

--- tools/dbutils/json.go
+++ tools/dbutils/json.go
@@ -2,12 +2,20 @@ package dbutils
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
 // JSONEach returns JSON_EACH SQLite string expression with
 // some normalizations for non-json columns.
 func JSONEach(column string) string {
+	if strings.EqualFold(os.Getenv("PB_DATABASE_DRIVER"), "mysql") {
+		return fmt.Sprintf(
+			`JSON_TABLE(CASE WHEN JSON_VALID([[%s]]) AND JSON_TYPE([[%s]]) = 'ARRAY' THEN [[%s]] ELSE JSON_ARRAY([[%s]]) END, '$[*]' COLUMNS(value VARCHAR(255) PATH '$'))`,
+			column, column, column, column,
+		)
+	}
+
 	// note: we are not using the new and shorter "if(x,y)" syntax for
 	// compatibility with custom drivers that use older SQLite version
 	return fmt.Sprintf(
